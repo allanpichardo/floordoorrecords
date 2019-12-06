bbb2b0f2e1f555c7536c117de946fba66ea67265
Allan Pichardo <allan.pichardo@gmail.com>
2019-12-06 00:27:18 -0500

    Updates

---
+++ precache-manifest.0b658d46de78d17c5894bfd10d08321f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daa4c67ef05b209dca3b03b05dcde88",
+    "url": "/floordoorrecords/index.html"
+  },
+  {
+    "revision": "433440968f4c952b0bd3",
+    "url": "/floordoorrecords/static/css/main.8259ac17.chunk.css"
+  },
+  {
+    "revision": "f576ead783b55c4857ff",
+    "url": "/floordoorrecords/static/js/2.b27a4868.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/floordoorrecords/static/js/2.b27a4868.chunk.js.LICENSE"
+  },
+  {
+    "revision": "433440968f4c952b0bd3",
+    "url": "/floordoorrecords/static/js/main.394e4487.chunk.js"
+  },
+  {
+    "revision": "8b9fc03c16f66d037956",
+    "url": "/floordoorrecords/static/js/runtime-main.c3dcaddf.js"
+  },
+  {
+    "revision": "222aeaaf363c1fbacfcdfa22950c2d3e",
+    "url": "/floordoorrecords/static/media/floordoor_logo.222aeaaf.svg"
+  }
+]);
